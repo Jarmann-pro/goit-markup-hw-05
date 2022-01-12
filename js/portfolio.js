39beae6910b8b55cef55690bcfd063f6dfcff15e
Nikita <obodzinskiy.nikita@gmail.com>
2022-01-12 10:04:21 +0200

    5.2

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('button[type="logic_button"]').onclick = function() {
-    document.querySelectorAll('button[type="logic_button"]').classList.add('active_logic_button');
-    document.querySelectorAll('button[type="logic_button"]').classList.remove('non_active_logic_button');
+document.addEventListener('[logic_button]').onClick = function() {
+    document.querySelector('[logic_button]').classList.add('active_logic_button');
+    document.querySelector('[logic_button]').classList.remove('non_active_logic_button');
 }
